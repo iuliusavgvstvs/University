41fb06c58df01ae39f702e1fcae42ee680848ad0
iuliusavgvstvs <iulius.satmari@gmail.com>
2020-03-29 13:43:21 +0300

    model + db config

---
+++ Design and Programming Environments/Atletism/Atletism GUI .NET/model/Copil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atletism_GUI.NET.model
+{
+    class Copil:IEntity
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Copil(string fName, string lName, int nAge)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+            this.age = nAge;
+        }
+
+        public int Id { get; set; }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0} {1}, age: {2}", firstName, lastName, age);
+        }
+    }
+}
---
+++ Design and Programming Environments/Atletism/Atletism GUI .NET/model/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Atletism_GUI.NET.model
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
